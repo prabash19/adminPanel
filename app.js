c601af94e6a93688adadc06156e5dc31143b14af
suprabash <suprabash@esignature.com.np>
2023-03-16 18:34:29 +0545

    initial setup

---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const errorMiddleware = require("./middleware/error");
+const ErrorHandler = require("./utils/errorHandler");
+dotenv.config({ path: "./config.env" });
+
+const app = express();
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", true);
+connectDB(DB);
+async function connectDB(DB) {
+  try {
+    await mongoose.connect(DB, {
+      useNewUrlParser: true,
+    });
+    console.log("DB connected");
+  } catch (err) {
+    console.log("DB error: ", err);
+  }
+}
+
+// Error handler for invalid routes
+app.all("*", (req, res, next) => {
+  next(new ErrorHandler("Cannot find requested route", 404));
+});
+
+app.use(errorMiddleware);
+
+module.exports = app;
---
+++ utils/apiFeatures.js
@@ -0,0 +1,44 @@
+class ApiFeatures {
+  constructor(query, queryString) {
+    (this.query = query), (this.queryString = queryString);
+  }
+
+  search() {
+    const product = this.queryString.product
+      ? {
+          name: {
+            $regex: this.queryString.product,
+            $options: "i",
+          },
+        }
+      : {};
+    this.query = this.query.find({ ...product });
+    return this;
+  }
+
+  filter() {
+    const queryStringCopy = { ...this.queryString };
+    // removing fields in query that is not for filter
+    const removeFields = ["keyword", "limit", "page"];
+    removeFields.forEach((el) => {
+      delete queryStringCopy[el];
+    });
+    // removing fields in query that is not for filter
+
+    // creating filter for price and rating
+    let queryStr = JSON.stringify(queryStringCopy);
+    queryStr = queryStr.replace(/\b(gt|gte|lt|lte)\b/g, (key) => `$${key}`);
+
+    this.query = this.query.find(JSON.parse(queryStr));
+    return this;
+  }
+
+  pagination(resultsPerPage) {
+    const currentPage = Number(this.queryStr.page) || 1;
+    const skip = resultsPerPage * (currentPage - 1);
+    this.query = this.query.limit(resultsPerPage).skip(skip);
+    return this;
+  }
+}
+
+module.exports = ApiFeatures;
